3c5adb7a11bc9e3e0b2feb2c6bef87f70809ca02
francesco <francesco.saverio.zuppichini@ch.pwc.com>
2019-12-04 15:06:51 +0100

    minor changes

--- source/data/VideoDataset.py
+++ source/data/VideoDataset.py
@@ -1,8 +1,23 @@
+import pytube
+import numpy as np
 from torch.utils.data import Dataset
+from pathlib import Path
+from logger import logging
 
 class VideoDataset(Dataset):
+    def __init__(self, frames: np.array):
+        self.frames = frames
+
     def __getitem__(self, idx):
         pass
 
     def __len__(self):
         pass
+
+    @classmethod
+    def from_yt(csl, video_url: str, out_dir: Path):
+        logging.info(f"Downloading data from {video_url}")
+        if not out_dir.exists(): out_dir.mkdir()
+        frames = []
+        return VideoDataset(frames)
+
