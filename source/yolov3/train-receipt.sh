91b17f9b8ce65ec4d43b579e379d93f1fccb6465
francesco <francesco.saverio.zuppichini@ch.pwc.com>
2019-12-10 20:55:04 +0100

    asd

--- source/yolov3/train-receipt.sh
+++
@@ -1,3 +0,0 @@
-python3 train.py --data ../receipt/receipt.data --cfg cfg/yolov3-receipt.cfg --weights weights/yolov3-spp.weights --transfer
-
-python3 train.py --data ../receipt/receipt.data --cfg cfg/yolov3-tiny-receipt.cfg --weights weights/yolov3-tiny.weights --transfer
